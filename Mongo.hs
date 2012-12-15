71521e64281f165f90eba08b1922808ed8039bdd
Enguerrand Decorne <decorne.en@gmail.com>
2012-12-15 21:43:06 +0100

    Improve Mongo.hs code

--- Mongo.hs
+++ Mongo.hs
@@ -2,7 +2,6 @@
 module Mongo(
 initMongoCo
 , mongoRun
-, lookupData
 , valueToString
 , queryMetadataById
 , checkResponse
@@ -15,22 +14,20 @@ import Data.Maybe as Maybe
 
 initMongoCo = runIOE $ MongoDB.connect $ MongoDB.host "127.0.0.1"
 
-mongoRun pipe act = MongoDB.access pipe MongoDB.master "cotonnier" act
+mongoRun pipe = MongoDB.access pipe MongoDB.master "cotonnier"
 
 valueToString :: Maybe String -> String
 valueToString (Just a) = a
 valueToString Nothing = "Error xd"
 
-lookupData field doc = MongoDB.look field doc
-
 getString :: Either String (Maybe String) -> String
 getString (Left error) = error
-getString (Right (Nothing)) = "Value error"
+getString (Right Nothing) = "Value error"
 getString (Right (Just value)) = value
 
 getValue :: (Val a) => Either String Document -> Label -> Either String (Maybe a)
 getValue (Left error) _ = Left error
-getValue (Right document) field = Right val where val = getval $ lookupData field document
+getValue (Right document) field = Right val where val = getval $ MongoDB.look field document
                                                   getval Nothing = Nothing
                                                   getval (Just a) = MongoDB.cast' a
 
@@ -41,7 +38,7 @@ queryMetadataById id = do
   let document = checkResponse response
   return document
 
-checkResponse :: (Either Failure (Maybe Document)) -> (Either String Document)
+checkResponse :: Either Failure (Maybe Document) -> Either String Document
 checkResponse (Left _) = Left "Error while querying MongoDB"
 checkResponse (Right Nothing) = Left "Querying return nothing"
 checkResponse (Right (Just document)) = Right document
