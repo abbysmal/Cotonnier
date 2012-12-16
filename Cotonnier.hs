27651e317417f3a5d6fb19aef7e109420a58a417
Enguerrand Decorne <decorne.en@gmail.com>
2012-12-16 13:41:57 +0100

    Add static file loading

--- Cotonnier.hs
+++ Cotonnier.hs
@@ -2,6 +2,8 @@
 
 import Yesod as Yesod
 import Mongo as Mongo
+import Yesod.Markdown as Markdown
+
 data Cotonnier = Cotonnier
 
 mkYesod "Cotonnier" [parseRoutes|
@@ -11,17 +13,21 @@ mkYesod "Cotonnier" [parseRoutes|
 
 instance Yesod Cotonnier
 
+getStaticArticle id = "/home/engil/static/" ++ (show id) ++ "/article.md"
+
 getHomeR :: Handler Yesod.RepHtml
 getHomeR = Yesod.defaultLayout [whamlet|Accueil|]
 
 getCotonsIdR :: Integer -> Handler Yesod.RepHtml
 getCotonsIdR id = do
   query <- Yesod.liftIO $ Mongo.queryMetadataById id
+  corpusmd <- Yesod.liftIO $ Markdown.markdownFromFile (getStaticArticle id)
   let metadatas = query
       author = Mongo.getString $ getValue metadatas "author"
       tags = Mongo.getString $ getValue metadatas "tags"
       date = Mongo.getString $ getValue  metadatas "date"
       title = Mongo.getString $ getValue  metadatas "title"
+      corpus = Markdown.markdownToHtml
   Yesod.defaultLayout $(Yesod.whamletFile "Post.hamlet")
 
 main :: IO ()
