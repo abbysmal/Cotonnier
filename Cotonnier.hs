189bf9ace045ab353a346f8aa228d8432213c98c
Thomas Papillon <thooms.pap@gmail.com>
2013-01-03 21:53:14 +0100

    Comments now work :)

--- Cotonnier.hs
+++ Cotonnier.hs
@@ -5,6 +5,7 @@ import Mongo as Mongo
 import Yesod.Markdown as Markdown
 import Database.MongoDB as MongoDB
 import Yesod.Static
+import Control.Applicative ((<$>), (<*>))
 import Data.Text
 
 data Cotonnier = Cotonnier { getStatic :: Static }
@@ -13,13 +14,16 @@ staticFiles "static"
 
 mkYesod "Cotonnier" [parseRoutesNoCheck|
 / HomeR GET
-/#Integer CotonsIdR GET
+/#Integer CotonsIdR GET POST
 /author/#String AuthorR GET
 /tags/#String TagsR GET
 /static    StaticR Static getStatic
 |]
 
 instance Yesod Cotonnier
+
+instance RenderMessage Cotonnier FormMessage where
+    renderMessage _ _ = defaultFormMessage
 
 getStaticArticle id = "/home/thomas/dev/Haskell/Cotonnier/articles/" ++
                       show id ++ "/article.md"
@@ -37,13 +41,37 @@ getHomeR = do
   entries <- Yesod.liftIO $ Mongo.queryDocumentsWith [] "cotons" 10
   createPage $(Yesod.whamletFile "Home.hamlet")
 
+data Comment = Comment 
+               { name :: Text
+               , content :: Text
+               } deriving Show
+
+commentForm :: Html -> MForm Cotonnier Cotonnier (FormResult Comment, Widget)
+commentForm = renderDivs $ Comment
+              <$> areq textField "Name" Nothing
+              <*> areq textField "Content" Nothing
+
 getCotonsIdR :: Integer -> Handler Yesod.RepHtml
 getCotonsIdR id = do
+  corpusmd <- Yesod.liftIO $ Markdown.markdownFromFile (getStaticArticle id)
   metadatas <- Yesod.liftIO $ Mongo.queryDocumentWith ["id" =: id] "cotons"
   comments <- Yesod.liftIO $ Mongo.queryDocumentsWith ["id" =: id] "com" 20
-  corpusmd <- Yesod.liftIO $ Markdown.markdownFromFile (getStaticArticle id)
+  (widget, enctype) <- generateFormPost commentForm
   createPage $(Yesod.whamletFile "Post.hamlet")
 
+postCotonsIdR :: Integer -> Handler Yesod.RepHtml
+postCotonsIdR id = do
+  ((result, _), _) <- runFormPost commentForm
+  Yesod.liftIO $ case result of
+    FormSuccess comment -> 
+      insertComment id (name comment) (content comment)
+    _ -> return $ Left $ QueryFailure 42 "Fail." -- FIXME
+  corpusmd <- Yesod.liftIO $ Markdown.markdownFromFile (getStaticArticle id)
+  metadatas <- Yesod.liftIO $ Mongo.queryDocumentWith ["id" =: id] "cotons"
+  comments <- Yesod.liftIO $ Mongo.queryDocumentsWith ["id" =: id] "com" 20
+  (widget, enctype) <- generateFormPost commentForm
+  createPage $(Yesod.whamletFile "Post.hamlet")
+  
 getAuthorR :: String -> Handler Yesod.RepHtml
 getAuthorR author = do
   entries <- Yesod.liftIO $ queryDocumentsWith ["author" =: author] "cotons" 0
