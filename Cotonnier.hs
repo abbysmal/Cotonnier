3d382337e55dddd002d6e3445793a36fe4a7a0f2
Enguerrand Decorne <decorne.en@gmail.com>
2013-08-20 23:04:30 +0200

    Add Epub support

--- Cotonnier.hs
+++ Cotonnier.hs
@@ -3,16 +3,25 @@
 {-# LANGUAGE OverloadedStrings, ExtendedDefaultRules #-}
 {-# LANGUAGE StandaloneDeriving, GeneralizedNewtypeDeriving #-}
 
+import Yesod.Static (StaticRoute, Route(StaticRoute), Static, static)
+import Yesod (parseRoutes)
 import Control.Applicative ((<$>), (<*>))
 import Database.MongoDB ((=:))
-import Yesod.Static (StaticRoute, Route(StaticRoute), Static, static)
 import qualified Yesod as Yesod
 import qualified Yesod.Static as Static
 import qualified Mongo as Mongo
-import qualified Yesod.Markdown as Markdown
 import qualified Database.MongoDB as MongoDB
 import qualified Data.Text as Text
-import Yesod (parseRoutes)
+import qualified Yesod.Markdown as Markdown
+import qualified Text.Pandoc as Pandoc
+import qualified Yesod.Static as Static
+import qualified Yesod as Yesod
+import qualified Yesod.Core.Content as Content
+import Mongo as Mongo
+
+
+-- Basic definitions and routing informations for the application
+
 
 data Cotonnier = Cotonnier { getStatic :: Static.Static }
 
@@ -24,6 +33,7 @@ Yesod.mkYesod "Cotonnier" [parseRoutes|
 /author/#String AuthorR GET
 /tags/#String TagsR GET
 /static    StaticR Static getStatic
+/epub EpubR GET
 |]
 
 instance Yesod.Yesod Cotonnier
@@ -35,6 +45,41 @@ getStaticArticle :: Show a => a -> String
 getStaticArticle id = "/articles/paths/" ++
                       show id ++ "/article.md"
 
+
+-- Epub stuff
+
+
+newtype RepEpub = RepEpub Content.Content
+
+epubToContent = return . Content.toContent
+
+epubToRepEpub = fmap RepEpub . epubToContent
+
+typeEpub :: Content.ContentType
+typeEpub = "application/epub+zip"
+
+repEpub :: Content.ToContent a => a -> RepEpub
+repEpub = RepEpub . Content.toContent
+
+
+instance Content.HasContentType RepEpub where
+      getContentType _ = typeEpub
+
+deriving instance Content.ToContent RepEpub
+
+instance Content.ToTypedContent RepEpub where
+      toTypedContent (RepEpub c) = Content.TypedContent typeEpub c
+
+
+-- Functions used by the handlers
+
+
+readMarkdownFromFile id = do
+  file <- Prelude.readFile (getStaticArticle id)
+  let md = Pandoc.readMarkdown (Pandoc.def Pandoc.ReaderOptions) file
+  epub <- Pandoc.writeEPUB (Pandoc.def Pandoc.WriterOptions) md
+  return epub
+
 createPage content =
   Yesod.defaultLayout $ do
     Yesod.setTitle "Cotonnier"
@@ -42,21 +87,25 @@ createPage content =
     Yesod.addStylesheet $ StaticR cotonnier_css
     content
 
-getHomeR :: Handler Yesod.RepHtml
-getHomeR = do
-  entries <- Yesod.liftIO $ Mongo.queryDocumentsWith [] "cotons" 10
-  createPage $(Yesod.whamletFile "Home.hamlet")
+commentForm = Yesod.renderDivs $ Comment
+              <$> Yesod.areq Yesod.textField "Name" Nothing
+              <*> Yesod.areq Yesod.textareaField "Content" Nothing
 
 data Comment = Comment
                { name :: Text.Text
                , content :: Yesod.Textarea
                } deriving Show
 
-commentForm = Yesod.renderDivs $ Comment
-              <$> Yesod.areq Yesod.textField "Name" Nothing
-              <*> Yesod.areq Yesod.textareaField "Content" Nothing
 
-getCotonsIdR :: Integer -> Handler Yesod.RepHtml
+-- The handlers themselves
+
+
+getHomeR :: Handler Yesod.Html
+getHomeR = do
+  entries <- Yesod.liftIO $ Mongo.queryDocumentsWith [] "cotons" 10
+  createPage $(Yesod.whamletFile "Home.hamlet")
+
+getCotonsIdR :: Integer -> Handler Yesod.Html
 getCotonsIdR id = do
   corpusmd <- Yesod.liftIO $ Markdown.markdownFromFile (getStaticArticle id)
   metadatas <- Yesod.liftIO $ Mongo.queryDocumentWith ["id" =: id] "cotons"
@@ -64,7 +113,7 @@ getCotonsIdR id = do
   (widget, enctype) <- Yesod.generateFormPost commentForm
   createPage $(Yesod.whamletFile "Post.hamlet")
 
-postCotonsIdR :: Integer -> Handler Yesod.RepHtml
+postCotonsIdR :: Integer -> Handler Yesod.Html
 postCotonsIdR id = do
   ((result, _), _) <- Yesod.runFormPost commentForm
   case result of -- Ignoring all errors
@@ -74,16 +123,25 @@ postCotonsIdR id = do
     _ -> return ()
   getCotonsIdR id
 
-getAuthorR :: String -> Handler Yesod.RepHtml
+getAuthorR :: String -> Handler Yesod.Html
 getAuthorR author = do
   entries <- Yesod.liftIO
                     $ Mongo.queryDocumentsWith ["author" =: author] "cotons" 0
   createPage $(Yesod.whamletFile "Home.hamlet")
 
-getTagsR :: String -> Handler Yesod.RepHtml
+getTagsR :: String -> Handler Yesod.Html
 getTagsR tag = do
   entries <- Yesod.liftIO $ Mongo.queryDocumentsWith ["tags" =: tag] "cotons" 0
   createPage $(Yesod.whamletFile "Home.hamlet")
+
+getEpubR :: Handler RepEpub
+getEpubR = do
+  document <- Yesod.liftIO $ readMarkdownFromFile 1
+  (epubToRepEpub document)
+
+
+-- Main function, launch the webserver and starts the application
+
 
 main :: IO ()
 main = do
